5db2db718c1216f8c8c88c0fbf2e85500a6f3e5d
pauliee99 <pavlosan67@gmail.com>
2024-10-23 11:16:34 +0300

    spring initlzr

---
+++ ed-demo/src/main/java/com/example/ed_demo/EdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ed_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdDemoApplication.class, args);
+	}
+
+}
---
+++ ed-demo/src/test/java/com/example/ed_demo/EdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ed_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
